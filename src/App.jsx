d82cbabb8cf92fc59b2d9e79cb24ed073d0e39fc
github_username <nedimmuminovic61@gmail.com>
2024-05-01 15:43:49 +0200

    using and outputing dynamic values

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+const descriptions = ['fundamental', 'core', 'crucial']
+
+function random(max){
+  return Math.floor(Math.random() * (max + 1))
+}
+
+
 function Header(){
+   const des = descriptions[random(2)]
+
   return(
   <header>
   <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
   <h1>React Essentials</h1>
   <p>
-    Fundamental React concepts you will need for almost any app you are
+    {des} React concepts you will need for almost any app you are
     going to build!
   </p>
 </header>)
