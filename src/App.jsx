7dd7908f61918140cc4a9bcbcfb3c5183834892a
github_username <nedimmuminovic61@gmail.com>
2024-05-01 16:02:26 +0200

    better way of displaying images

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from "./assets/react-core-concepts.png"
+
 const descriptions = ['fundamental', 'core', 'crucial']
 
 function random(max){
@@ -10,7 +12,7 @@ function Header(){
 
   return(
   <header>
-  <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+  <img src={reactImg} alt="Stylized atom" />
   <h1>React Essentials</h1>
   <p>
     {des} React concepts you will need for almost any app you are
